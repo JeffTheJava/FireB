52c9df935a80c5fb8bff1dcbfd853afe9984f59c
JeffTheJava <JeffTheJava@gmail.com>
2016-09-08 21:52:57 -0400

    stop the phone from sleeping

--- AndroidApp/FireB/app/src/main/java/com/litmantech/fireb/MainActivity.java
+++ AndroidApp/FireB/app/src/main/java/com/litmantech/fireb/MainActivity.java
@@ -2,6 +2,7 @@ package com.litmantech.fireb;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.WindowManager;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);//don't let phone sleep
+
     }
 }
